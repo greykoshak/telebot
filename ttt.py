58177bcf5d5a808a27001bc42cb0ec38a78abb20
Igor Agarkov <igor.agarkov.007@gmail.com>
2019-03-25 17:01:21 +0200

    Telebot v1.2

--- ttt.py
+++ ttt.py
@@ -5,10 +5,16 @@ bot = telebot.TeleBot(token)
 
 currencies = ['евро', 'доллар']
 
+def closeset_bank(location):
+    lat = location.latitude
+    lon = location.longitude
+    bank_address = 'ул. Гринченка, 9'
+    bank_lat, bank_lon = 55.800388, 37.543710
+    return bank_address, bank_lat, bank_lon
 
 def check_currency(message):
     for c in currencies:
-        if c in message.text.lower():
+        if message.text is not None and c in message.text.lower():
             return True
     return False
 
@@ -21,6 +27,7 @@ def check_currency_value(text):
     return None, None
 
 
+@bot.message_handler(commands=['rate'])
 @bot.message_handler(func=check_currency)
 def handle_currency(message):
     print(message.text)
@@ -30,6 +37,12 @@ def handle_currency(message):
     else:
         bot.send_message(chat_id=message.chat.id, text='Узнай курс валют.')
 
+@bot.message_handler(content_types=['location'])
+def handle_location(message):
+    print(message.location)
+    bank_address, bank_lat, bank_lon = closeset_bank(message.location)
+    bot.send_message(chat_id=message.chat.id, text='Ближайший банк {}'.format(bank_address))
+    bot.send_location(message.chat.id, bank_lat, bank_lon)
 
 @bot.message_handler()
 def handle_message(message):
